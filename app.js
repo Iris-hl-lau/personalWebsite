d48791980e23233d5816590558648e57d7f224fd
Iris-hl-lau <98.iris.lau@gmail.com>
2019-03-06 21:44:41 -0800

    commit

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 let http = require('http');
-let date = new Date();
-
+let url = require("url");  
+let fs = require('fs'); 
+// let express = require('express');
+// let app = express();
 let port = process.env.PORT || 8080
+
 http.createServer((request, response) => {
-response.writeHead(200,{'Content-type': 'text/plain'});
-//date.setUTCHours(date.getHours() - 8);
-response.end(String(date));
+    let path = url.parse(request.url).pathname;  
+
+    if(path == "/") {
+        fs.readFile('./index.html' + path, function(error, data) {  
+            if (error) {  
+                response.writeHead(404);  
+                response.write(error);  
+                response.end();  
+            } else {  
+                response.writeHead(200, {  
+                    'Content-Type': 'text/html'  
+                });  
+                response.write(data);  
+                response.end();  
+            }  
+        });  
+    } else {
+        fs.readFile(__dirname + path, function(error, data) {  
+            if (error) {  
+                response.writeHead(404);  
+                response.write(error);  
+                response.end();  
+            } else {  
+                response.writeHead(200, {  
+                    'Content-Type': 'text/html'  
+                });  
+                response.write(data);  
+                response.end();  
+            }  
+        });  
+    }
+
+    
 }).listen(port,() => {
 console.log('listening...');
 });
